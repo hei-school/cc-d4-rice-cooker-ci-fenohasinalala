e566082f8a1f5399279ab4b620923931b9a20a8d
Lova Fenohasina RAFANOMEZANTSOA <fenohasinalala@gmail.com>
2023-12-17 19:42:15 +0300

    refactor: create constants for menu text

--- RiceCookerView.py
+++ RiceCookerView.py
@@ -4,6 +4,23 @@ from Validation import Validation, get_valid_number_input
 
 
 class RiceCookerView:
+    MAIN_MENU = (
+        "",
+        "MAIN MENU:",
+        "1 - COOK FOOD",
+        "2 - STEAM FOOD",
+        "3 - CHECK STATUS",
+        "4 - SHUT DOWN",
+    )
+
+    COOK_FOOD_MENU = (
+        "",
+        "COOK FOOD MENU:",
+        "1 - RICE",
+        "2 - FOLLOW RECIPES",
+        "3 - Return to MAIN MENU",
+    )
+
     scanner = None
 
     def __init__(self, service):
@@ -22,7 +39,6 @@ class RiceCookerView:
                     self.menu_prompt_msg(min_val, max_val), min_val, max_val
                 )
             )
-
             if Validation.is_cooking(rice_cooker) and choice == 1:
                 continue
             match choice:
@@ -43,20 +59,10 @@ class RiceCookerView:
         return f"Enter your choice ({min_val} - {max_val}): "
 
     def display_main_menu(self):
-        print()
-        print("MAIN MENU:")
-        print("1 - COOK FOOD")
-        print("2 - STEAM FOOD")
-        print("3 - CHECK STATUS")
-        print("4 - SHUT DOWN")
+        print(*self.MAIN_MENU, sep="\n")
 
     def cook_food_menu(self, rice_cooker):
-        print()
-        print("COOK FOOD MENU:")
-        print("1- RICE")
-        print("2- FOLLOW RECIPES")
-        print("3- Return to MAIN MENU")
-
+        print(*self.COOK_FOOD_MENU, sep="\n")
         min_val = 1
         max_val = 3
         choice = int(
