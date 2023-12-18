1317585b146533b5be83074df8ccd1bcfb681345
Lova Fenohasina RAFANOMEZANTSOA <fenohasinalala@gmail.com>
2023-12-18 17:48:54 +0300

    chore: add formatter and linter

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    node: true,
+    es6: true,
+    commonjs: true,
+    mocha: true,
+  },
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  parserOptions: {
+    ecmaVersion: 2022, // or a higher version that your project supports
+  },
+};
